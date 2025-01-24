83c20878312e118bde3536ec07a93c9323948280
terminaate <tntwnik@gmail.com>
2025-01-24 17:09:26 +0300

    Adds Canvas dependency and refactors banner creation to use the new `Banner` class.

--- src/banner/updateBanner.ts
+++ src/banner/updateBanner.ts
@@ -1,23 +1,26 @@
 import { Activity, ActivityType, GuildMember } from 'discord.js';
 import { redisClient } from '@/redis';
-import { renderBanner } from '@/banner/renderBanner';
+import { Banner } from '@/banner/Banner';
+import { UserDTO } from '@/dto/user.dto';
+import { UserActivityDTO } from '@/dto/user-activity.dto';
 
 export const updateBanner = async (
 	member: GuildMember,
 	activities?: Activity[],
 ) => {
-	await redisClient.set(String(member.id), await renderBanner());
+	const activity = activities?.find((o) => o.type !== ActivityType.Custom);
 
-	// const activity = activities?.find((o) => o.type !== ActivityType.Custom);
-	//
-	// const canvas = await Banner.create(
-	// 	await UserDTO.create(member),
-	// 	activity ? new UserActivityDTO(activity) : undefined,
-	// );
-	// userBanners[member.id] = canvas.createPNGStream();
-	// 	userBanners[member.id] = `
-	// <svg width="${canvas.width}" height="${canvas.height}" xmlns="http://www.w3.org/2000/svg">
-	//   <image href="${dataUrl}" height="${canvas.height}" width="${canvas.width}" />
-	// </svg>
-	// `;
+	const canvas = await Banner.create(
+		await UserDTO.create(member),
+		activity ? new UserActivityDTO(activity) : undefined,
+	);
+
+	await redisClient.set(
+		String(member.id),
+		`
+	<svg width="${canvas.width}" height="${canvas.height}" xmlns="http://www.w3.org/2000/svg">
+	  <image href="${canvas.toDataURL()}" height="${canvas.height}" width="${canvas.width}" />
+	</svg>
+	`,
+	);
 };
