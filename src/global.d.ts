98028454c5623993409129fd5b58d1d06e56115d
terminaate <tntwnik@gmail.com>
2025-03-15 17:09:33 +0300

    Configures default Redis connection details and introduces a fake profile API environment variable.

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,12 +1,13 @@
 type Class<T = any> = { new (): T };
 
 namespace NodeJS {
-	interface ProcessEnv {
-		SERVER_PORT: string;
-		NODE_ENV: 'dev' | 'prod';
-		BOT_TOKEN: string;
-		GUILD_ID: string;
-		REDIS_HOST: string;
-		REDIS_PORT: string;
-	}
+  interface ProcessEnv {
+    SERVER_PORT: string;
+    NODE_ENV: 'dev' | 'prod';
+    BOT_TOKEN: string;
+    GUILD_ID: string;
+    REDIS_HOST: string;
+    REDIS_PORT: string;
+    FAKE_PROFILE_API: string;
+  }
 }
