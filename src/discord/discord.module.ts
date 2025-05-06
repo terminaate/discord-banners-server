84a4281e0d018e80f2c4d1053a35ea6962e8b14d
terminaate <terminaatecorp@gmail.com>
2025-05-06 16:34:13 +0300

    Using ConfigService in DiscordModule now

--- src/discord/discord.module.ts
+++ src/discord/discord.module.ts
@@ -1,19 +1,23 @@
-import { forwardRef, Module } from '@nestjs/common';
-import { DiscordService } from './discord.service';
 import { BannerModule } from '@/banner/banner.module';
-import { NecordModule } from 'necord';
+import { forwardRef, Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { GatewayIntentBits } from 'discord.js';
+import { NecordModule } from 'necord';
+import { DiscordService } from './discord.service';
 
 @Module({
   imports: [
-    NecordModule.forRoot({
-      token: process.env.BOT_TOKEN,
-      intents: [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMembers,
-        GatewayIntentBits.GuildPresences,
-        GatewayIntentBits.GuildMessages,
-      ],
+    NecordModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        token: configService.getOrThrow('BOT_TOKEN'),
+        intents: [
+          GatewayIntentBits.Guilds,
+          GatewayIntentBits.GuildMembers,
+          GatewayIntentBits.GuildPresences,
+          GatewayIntentBits.GuildMessages,
+        ],
+      }),
     }),
     forwardRef(() => BannerModule),
   ],
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,17 @@
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: true,
   });
+  const configService = app.get<ConfigService>(ConfigService);
+
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
-  const PORT = process.env.SERVER_PORT ?? 3000;
+  const PORT = +configService.get('PORT', 3000);
 
   await app.listen(PORT);
 
